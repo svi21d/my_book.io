acf95324df29e9b9a944cf6f7a27083e8bde74a4
Vitaliy Smirnov <luchshiy_1488@mail.ru>
2025-11-11 10:43:24 +0300

    background

--- шапка_подвал/head.php
+++ шапка_подвал/head.php
@@ -10,5 +10,10 @@
     <link rel="stylesheet" href="../css/for-example.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body{
+            background-color: antiquewhite;
+        }
+    </style>
 </head>
 <body>
