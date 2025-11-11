0a072288391d73688afe9a2293ab9e47eedf4efe
Vitaliy Smirnov <luchshiy_1488@mail.ru>
2025-11-11 10:53:42 +0300

    rename-title

--- шапка_подвал/head.php
+++ шапка_подвал/head.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>new pages</title>
     <link rel="stylesheet" href="../css/css_index.css">
     <link rel="stylesheet" href="../css/for-example.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
